12fca9c8b4298b9250d9679a4614410eead0c5ca
yangchufa591 <yangchufa591@pingan.com>
2023-05-08 11:07:15 +0800

    新增websocket支持

--- server/app.js
+++ server/app.js
@@ -2,11 +2,32 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+// 导入nodejs-websocket模块
+const io = require("nodejs-websocket");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+// 执行websocket处理连接方法
+io.createServer((connection) => {
+  console.log("new connection...");
+  //处理客户端发送过来的消息
+  connection.on("text", function (data) {
+    console.log("接收到的客户端消息:" + data);
+    connection.sendText("服务器端返回数据:" + data);
+
+    //监听关闭
+    connection.on("close", function (code, reason) {
+      console.log("Connection closed");
+    });
+    //监听异常
+    connection.on("error", () => {
+      console.log("服务异常关闭...");
+    });
+  });
+}).listen(30002);
 
 app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
